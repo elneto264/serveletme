4f960050b105544d81b3fda9fe1c08ed26e7add4
Ernesto <ernestoster@gmail.com>
2020-04-22 12:38:13 +0200

    V9
    Debug en Settings cambiado a True, estaba en false

--- letmepark/settings.py
+++ letmepark/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vaads)onh+h$6_c8%fl&obuju!l!r_&emf&z7sij$!-tt3icr6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['buscador-letmepark.herokuapp.com']
 
