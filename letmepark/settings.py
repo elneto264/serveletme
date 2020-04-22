a7e6f7260062c9dc7efb3967305d9f4bb17dd09c
Ernesto Silva Languasco <ernestoster@gmail.com>
2020-04-22 11:09:09 +0200

    pass oculta

--- letmepark/settings.py
+++ letmepark/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
          },
         'NAME': 'letmepark',
         'CLIENT': {
-            'host': 'mongodb+srv://students:a428PmHV6DzfPMnk@mongodb-cluster-us-east-1-yuln1.mongodb.net/test?retryWrites=true&w=majority',
+            'host': 'mongodb+srv://<user>:<password>@mongodb-cluster-us-east-1-yuln1.mongodb.net/test?retryWrites=true&w=majority',
         }
     }
 } 
